4bd5969416966ab9ed2b2458b251b5af704a9a8d
levsthings <bearnode@gmail.com>
2017-09-05 09:25:50 +0300

    add all markup

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,33 +1,37 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html exposing (button, div, h1, h4, input, text)
+import Html.Attributes exposing (class, placeholder, type_)
 
 
 main =
     div []
         [ div []
-            [ div [ class "header has-text-centered" ]
-                [ h1 [ class "app-title" ] [ text "hashtagwars" ]
+            [ div [ class "htw-header has-text-centered" ]
+                [ h1 [ class "htw-app-title" ] [ text "hashtagwars" ]
                 ]
             ]
-        , div [ class "main-screen" ]
+        , div [ class "htw-main-screen" ]
             [ div
                 [ class "columns" ]
                 [ div [ class "column is-3 is-offset-3 has-text-centered" ]
-                    [ h4 [] [ text "First Hashtag" ]
+                    [ h4 []
+                        [ text "First Hashtag" ]
+                    , input
+                        [ class "input is-medium", placeholder "#hashtag", type_ "text" ]
+                        []
                     ]
                 , div [ class "column is-3 has-text-centered" ]
                     [ h4 [] [ text "Second Hashtag" ]
+                    , input [ class "input is-medium", placeholder "#hashtag", type_ "text" ] []
                     ]
                 ]
             , div [ class "has-text-centered" ]
-                [ h4 [] [ text "Fight!" ]
+                [ button [ class "htw-controls-button button is-medium" ] [ text "Fight!" ]
                 ]
             , div [ class "columns" ]
                 [ div [ class "column is-6 is-offset-3 has-text-centered" ]
-                    [ h4 [] [ text "Results!" ]
-                    ]
+                    []
                 ]
             ]
         ]
